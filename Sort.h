f453750680971f906657ae0d60957458566ac0e1
Yuxiang <cyx123chang@outlook.com>
2026-07-08 23:05:40 +0800

    Add new sorting functions to Sort.h
    Added new sorting functions and updated header guards.

--- Sort.h
+++ Sort.h
@@ -16,3 +16,13 @@ void AdjustDown(int* arr, int parent, int size);
 void HeapSort(int* arr, int n);
 
 void BubbleSort(int* arr, int n);
+
+int GetMidNum(int* arr, int left, int right);
+//Hoare
+int PartSort1(int* arr, int left, int right);
+//Pit-Digging Method
+int PartSort2(int* arr, int left, int right);
+//Front-Rear Pointers
+int PartSort3(int* arr, int left, int right);
+
+void QuickSort(int* arr, int left, int right);
